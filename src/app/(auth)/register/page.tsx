5b6ec026d44da6ff52b022facbd56bf98c6462c5
Muhammad Ihsan <ihsan@femaledaily.com>
2024-12-30 18:28:21 +0700

    feat: add registration page with form validation and UI components

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -18,19 +18,40 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const formSchema = z.object({
-  usernameOrEmal: z.string().nonempty({
-    message: "This input not empty",
-  }),
+  name: z
+    .string()
+    .nonempty({
+      message: "This input not empty",
+    })
+    .min(5, {
+      message: "min 5 character",
+    }),
+  email: z
+    .string()
+    .nonempty({
+      message: "This input not empty",
+    })
+    .email({ message: "format must be email" }),
+  username: z
+    .string()
+    .nonempty({
+      message: "This input not empty",
+    })
+    .min(4, {
+      message: "minimal 4 characters",
+    }),
   password: z.string().nonempty({
     message: "This input not empty",
   }),
 });
 
-export default function Login() {
+export default function Register() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      usernameOrEmal: "",
+      name: "",
+      email: "",
+      username: "",
       password: "",
     },
   });
@@ -46,22 +67,48 @@ export default function Login() {
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
               <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Login</h1>
+                <h1 className="text-2xl font-bold">Register</h1>
                 <p className="text-balance text-muted-foreground mt-2">
-                  Masak sangat menyenangkan!
+                  Ayo gabung ke komunitas memasak!
                 </p>
               </div>
 
               <FormField
                 control={form.control}
-                name="usernameOrEmal"
+                name="name"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="font-semibold">
-                      Username Or Email
-                    </FormLabel>
+                    <FormLabel className="font-semibold">Name</FormLabel>
                     <FormControl>
-                      <Input placeholder="me@mail.com or username" {...field} />
+                      <Input placeholder="your name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-semibold">Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="me@mail.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-semibold">Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="your username" {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -75,19 +122,23 @@ export default function Login() {
                   <FormItem>
                     <FormLabel className="font-semibold">Password</FormLabel>
                     <FormControl>
-                      <Input placeholder="your password" {...field} type="password" />
+                      <Input
+                        placeholder="your password"
+                        {...field}
+                        type="password"
+                      />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
               <Button className="w-full" type="submit">
-                Login
+                Register
               </Button>
               <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <Link href="/register" className="underline underline-offset-4">
-                  Register
+                You have an account?{" "}
+                <Link href="/login" className="underline underline-offset-4">
+                  Login
                 </Link>
               </div>
             </form>
